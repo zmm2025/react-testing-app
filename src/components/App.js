a453511c90a89bdc3a4c1f06a3b56f15aaf0fedb
Zain McCoy <zmm2025@gmail.com>
2024-06-11 16:39:02 -0400

    Worked on App, ClassSelector, and some styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,14 @@
 import logo from '../assets/images/logo.svg';
 import '../assets/styles/app.css';
 
+// Import components
+import ClassSelector from './ClassSelector';
+
 // Export App component
 export default function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClassSelector />
     </div>
   )
 }
---
+++ src/components/ClassSelector.js
@@ -0,0 +1,23 @@
+// Import assets
+import '../assets/styles/class-selector.css';
+
+export default function ClassSelector() {
+    return (
+        <table className="class-selector">
+            <thead>
+                <tr>
+                    <th>Level 1</th>
+                    <th>Level 2</th>
+                    <th>Level 3</th>
+                    <th>Level 4</th>
+                    <th>Level 5</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Value 1</th>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
