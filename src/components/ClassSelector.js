49af295c9ad767ea67abbd54a3714c1875a49b7e
Zain McCoy <zmm2025@gmail.com>
2024-06-12 18:45:11 -0400

    Normalized table body row population (this was so hard)

--- src/components/ClassSelector.js
+++ src/components/ClassSelector.js
@@ -24,9 +24,15 @@ export default function ClassSelector() {
                     <td>Value</td>
                     <td>Value</td>
                 </tr>
+                {bodyRows}
             </tbody>
         </table>
     )
+}
+
+// Define helper functions
+function childrenCount(object) {
+    return Object.keys(object).length;
 }
 
 // Define classes data
@@ -68,9 +74,33 @@ const classesData = {
     },
 }
 const classesDataDepth = 5;
+let currentClassPath = ["Site Products", "Ground Anchorages"];
 
 // Generate table header cells
 let headerCells = [];
 for (let levelIndex = 1; levelIndex <= classesDataDepth; levelIndex++) {
     headerCells.push(<th>Level {levelIndex}</th>);
+}
+
+// Generate table body rows
+let bodyRows = [];
+for (let rowIndex = 0; rowIndex < childrenCount(classesData); rowIndex++) {
+    // Generate table row cells by traversing selected classes indexing each cell's class
+    let rowCells = [];
+    let levelClass = classesData;
+    for (let levelIndex = 1; levelIndex <= classesDataDepth; levelIndex++) {
+        let cellText = "";
+        // If the cell's level is within the selected or next class,
+        // get its text and traverse to the next class if its level is selected
+        if (levelIndex <= currentClassPath.length + 1) {
+            cellText = Object.keys(levelClass)[rowIndex];
+            
+            if (levelIndex <= currentClassPath.length) {
+                let nextClass = currentClassPath[levelIndex - 1]
+                levelClass = levelClass[nextClass];
+            }
+        }
+        rowCells.push(<td>{cellText}</td>);
+    }
+    bodyRows.push(<tr>{rowCells}</tr>);
 }
