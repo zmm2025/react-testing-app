cd1ae94cda66c96a0c2ccc5450ebad0a3367de73
Zain McCoy <zmm2025@gmail.com>
2024-06-17 17:05:24 -0400

    Made progress on code refactoring (does not compile)

--- src/components/ClassSelector.js
+++ src/components/ClassSelector.js
@@ -1,8 +1,58 @@
-// Import assets
-import '../assets/styles/class-selector.css';
-import classesData from "../data/classes.json";
+import "../assets/styles/class-selector.css";
+import level1Classes from "../data/classes.json";
+import { useState } from "react";
 
 export default function ClassSelector() {
+    const classesDataDepth = 7; // TODO: make this dynamically calculated from JSON
+
+    const [classPath, setClassPath] = useState(["Site Products", "Ground Anchorages"]); // TEMP: Default test value
+    const [levels, setLevels] = useState(updateLevels(classPath));
+    
+    function updateClassPath(selectedClass, classLevelNum) {
+        let newClassPath = classPath.slice(classLevelNum - 0).push(selectedClass);
+        setClassPath(newClassPath);
+        updateLevels();
+    }
+    
+    function updateLevels() {
+        let newLevels = [];
+        
+        // Push level 1 to levels array
+        let levelClasses = level1Classes;
+        newLevels.push(Object.keys(levelClasses));
+
+        // Push all other selected levels to levels array
+        for (const pathClass of classPath) {
+            levelClasses = levelClasses[pathClass];
+            newLevels.push(Object.keys(levelClasses));
+        }
+
+        setLevels(newLevels);
+    }
+
+    function childrenCount(object) {
+        return Object.keys(object).length;
+    }
+
+    let selectorColumns = levels.map((level, colIndex) => {
+        const levelNum = colIndex + 1;
+        // CONTINUE HERE
+        
+        // let columnBodyCells = 
+
+        return (
+            <div className="column">
+                <div className="header">
+                    Level {levelNum}
+                </div>
+                <div className="horizontal-separator" />
+                <div className="body">
+                    {columnbodyCells}
+                </div>
+            </div>
+        );
+    });
+    
     return (
         <div className="class-selector">
             {selectorColumns}
@@ -10,25 +60,19 @@ export default function ClassSelector() {
     );
 }
 
-// Define helper functions
-function childrenCount(object) {
-    return Object.keys(object).length;
-}
-
-// Define classes data
-const classesDataDepth = 7;
-let currentClassPath = ["Site Products", "Ground Anchorages"];
-
 // Initialize class selector columns
 let selectorColumns = [];
-let levelClasses = classesData;
+
+let levelClasses = level1Classes;
 for (let colIndex = 0; colIndex < classesDataDepth; colIndex++) {
     let columnCells = [];
+
+    let levelNum = colIndex + 1;
 
     // Add header to column
     columnCells.push(
         <div className="header">
-            Level {colIndex + 1}
+            Level {levelNum}
         </div>
     );
 
@@ -42,7 +86,7 @@ for (let colIndex = 0; colIndex < classesDataDepth; colIndex++) {
     for (let rowIndex = 0; rowIndex < childrenCount(levelClasses); rowIndex++) {
         let rowClass = Object.keys(levelClasses)[rowIndex];
         columnBodyCells.push(
-            <button className="body-cell">
+            <button className="body-cell" onClick={updatePath(rowClass, levelNum)}>
                 {rowClass}
             </button>
         );
