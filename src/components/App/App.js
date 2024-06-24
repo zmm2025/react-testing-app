bd34ee6a7638d98aac4a3e61c20b53df4e466c30
Zain McCoy <zmm2025@gmail.com>
2024-06-24 10:24:46 -0400

    create files for class selector rewrite

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import ClassSelector from '../ClassSelector/ClassSelector';
+import ClassSelectorV2 from '../ClassSelectorV2/ClassSelectorV2';
 import './App.css';
 
 export default function App() {
     return (
         <div className="app">
-            <ClassSelector />
+            {/* <ClassSelector /> */}
+            <ClassSelectorV2 />
         </div>
     );
 }
---
+++ src/components/ClassSelectorV2/ClassSelectorV2.js
@@ -0,0 +1,7 @@
+import "./ClassSelectorV2.css";
+
+export default function ClassSelectorV2() {
+    return (
+        <div className="class-selector" />
+    )
+}
