e9bcbf0d0ab3485d466eb3b422265bd0d385b123
Zain McCoy <zmm2025@gmail.com>
2024-06-24 12:03:09 -0400

    create selector elements, column, and divider components

--- src/components/ClassSelectorV2/ClassSelectorV2.js
+++ src/components/ClassSelectorV2/ClassSelectorV2.js
@@ -1,7 +1,51 @@
+import { createContext, useContext } from "react";
 import "./ClassSelectorV2.css";
 
+const SelectedClassesContext = createContext([]);
+
 export default function ClassSelectorV2() {
+    const initialSelectedClasses = [];
+    
     return (
-        <div className="class-selector" />
+        <SelectedClassesContext.Provider value={initialSelectedClasses}>
+            <div className="class-selector">
+                <SelectorElements />
+            </div>
+        </SelectedClassesContext.Provider>
+    )
+}
+
+function SelectorElements() {
+    const selectedClasses = useContext(SelectedClassesContext);
+    const dividerOrientation = "vertical";
+    let levelNum = 1;
+    
+    return (
+        <>
+            <SelectorColumn levelNum={levelNum} />
+            {selectedClasses.map((_selectedClass, _selectedClassIndex) => {
+                levelNum++;
+                return (
+                    <>
+                        <Divider orientation={dividerOrientation} />
+                        <SelectorColumn key={levelNum} levelNum={levelNum} />
+                    </>
+                )
+            })}
+            <Divider orientation={dividerOrientation} />
+            <SelectorColumn levelNum={++levelNum} isHoverColumn={true} />
+        </>
+    )
+}
+
+function SelectorColumn({ levelNum, isHoverColumn = false }) {
+    return (
+        <div className="selector-column" />
+    )
+}
+
+function Divider({ orientation }) {
+    return (
+        <div className={"divider " + orientation} />
     )
 }
