96bea7343a6160a034fd0b3357cad7aca6ee1362
Zain McCoy <zmm2025@gmail.com>
2024-07-01 15:44:49 -0400

    work on class path text component

--- src/components/ConfirmationWindow/ConfirmationWindow.js
+++ src/components/ConfirmationWindow/ConfirmationWindow.js
@@ -1,10 +1,11 @@
+import { Fragment } from "react";
 import "./ConfirmationWindow.css";
 
 export default function ConfirmationWindow({ stagedClassPath, confirmClassPath }) {
     return (
         <div className="confirmation-window">
             <ContentPreview />
-            <ClassPathText />
+            <ClassPathText classPath={stagedClassPath}/>
             <BlameText />
             <ActionButtons />
         </div>
@@ -17,9 +18,22 @@ function ContentPreview() {
     );
 }
 
-function ClassPathText() {
+function ClassPathText({ classPath }) {
+    const cssClassName = "class-path-text";
+    const delimiterCSSClassName = "divider";
+    
     return (
-        null
+        <div className={cssClassName}>
+            {classPath.map((pathClass, index) => {
+                const isFirstClass = index !== 0;
+                return (
+                    <Fragment key={index}>
+                        {!isFirstClass && <p className={delimiterCSSClassName}> &gt; </p>} {/* "&gt;" == ">" */}
+                        <p>{pathClass.name}</p>
+                    </Fragment>
+                )
+            })}
+        </div>
     );
 }
 
